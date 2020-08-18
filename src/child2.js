43abfef8b6d6de11f8ec0d9c37ff14a6a0b3eabf
fayazkhan56 <68395705+fayazkhan56@users.noreply.github.com>
2020-08-18 00:44:43 -0700

    update

--- src/child2.js
+++ src/child2.js
@@ -1,17 +1,17 @@
 import React, { useContext, useReducer } from 'react';
 import numberReducer from './numberReducer';
-function Child() {
+function Child2() {
 let [state, dispatch]= useReducer(numberReducer,52);
     return (
       <div>
         child2 {state}
      
-        <button onClick={()=> {dispatch{(type:'INCREMENT')} } }>increment</button>
+        <button onClick={()=> {dispatch{(type:'INCREMENT' val=65)} } }>increment</button>
         <button onClick={()=> {dispatch{(type:'DECREMENT')} }}>decrement</button>
 
       </div>
     );
   }
-  
-  export default Child;
+   
+  export default Child2;
   
--- src/numberReducer.js
+++ src/numberReducer.js
@@ -1,10 +1,11 @@
 const numberReducer = (state, action) => {
-    switch (action) {
+    switch (action.type) {
       case 'INCREMENT':
-        return state + 1
+        return state + action.val
       case 'DECREMENT':
         return state - 1
-      default:
-        throw new Error()
+    
     }
   }
+
+  export default numberReducer;
